07dfbc82c5c42ac9b9adcf540c8049a788fe5508
Rossenzii <jangmj80@naver.com>
2025-05-21 10:10:38 +0900

    [Feat] init.db 파일 생성

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -1,12 +1,16 @@
-CREATE DATABASE IF NOT EXISTS infra;
-USE infra;
+DROP DATABASE IF EXISTS `infra`;
 
-CREATE TABLE IF NOT EXISTS room (
-  room_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE DATABASE `infra`;
+USE `infra`;
+
+CREATE TABLE IF NOT EXISTS `room` (
+  room_id int AUTO_INCREMENT PRIMARY KEY,
   room_number VARCHAR(50) NOT NULL
 );
 
 INSERT INTO room (room_number) VALUES
   ('101'),
   ('102'),
-  ('103');
+  ('103'),
+  ('104'),
+  ('105');
